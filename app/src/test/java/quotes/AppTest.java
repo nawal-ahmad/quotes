e8744203aa0d24db8754acf0ff56f757fdbbeb56
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-10-06 17:02:59 +0300

    get request to api

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -3,10 +3,16 @@
  */
 package quotes;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +30,9 @@ class AppTest {
         }
         assertEquals("Charles Dickens",quotes.get(1).getAuthor());
         assertEquals("18651 likes",quotes.get(0).getLikes());
+
+    }
+    @Test void jsonAPITest(){
 
     }
 }
